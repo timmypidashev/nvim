d700d227bb8ce810c400ffc5a28fa036f75ae672
Timothy Pidashev <pidashev.tim@gmail.com>
2024-08-20 18:23:56 -0700

    add lspconfig and cmp snippets

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local cmp = require('cmp')
+      require('luasnip.loaders.from_vscode').lazy_load()
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.complete(),
+          ['<C-o>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        }),
+      })
+    end,
+  },
+}
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    priority = 1000,
+    opts = {
+      ui = {
+        border = 'rounded',
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    },
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "clangd",
+        "tsserver",
+        "quick_lint_js",
+        "html",
+        "cssls"
+      }
+    },
+    config = true,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local on_attach = function(_, _)
+      -- no keybinds yet
+      end
+
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      }
+
+      lspconfig.tsserver.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
+
+      lspconfig.clangd.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
+    end,
+  },
+}
